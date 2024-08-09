15006c8c2944c26296ebc4e8996254135bf19cfe
Elizaveta <strel_liza_2008@mail.ru>
2024-08-09 16:51:43 +1200

    add prilogenie3

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,15 +1,5 @@
 public class BonusMilesService {
-    public int calculate(int price, int miles) {
-        int resalt;
-        if (price > miles) {
-            resalt = price;
-        } else {
-            resalt = miles;
-        }
-        return resalt;
-    }
-
     public int calculate(int price) {
-        return price;
+        return price / 20;
     }
 }
